11cfa715417b941027ff57e06bcf4cde74ded46c
NguyenVanTienManh <145360803+NguyenVanTienManh@users.noreply.github.com>
2023-10-25 23:06:35 +0700

    Update bt2.py

--- bt2.py
+++ bt2.py
@@ -1,20 +1,37 @@
 import cv2
 import numpy as np
-img = cv2.imread('pic2.png')
-cv2.imshow('Original', img)
-# generating the kernels
-kernel_sharpen_1 = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-kernel_sharpen_2 = np.array([[1,1,1], [1,-7,1], [1,1,1]])
-kernel_sharpen_3 = np.array([[-1,-1,-1,-1,-1],
- [-1,2,2,2,-1],
- [-1,2,8,2,-1],
- [-1,2,2,2,-1],
- [-1,-1,-1,-1,-1]]) / 8.0
-# applying different kernels to the input image
-output_1 = cv2.filter2D(img, -1, kernel_sharpen_1)
-output_2 = cv2.filter2D(img, -1, kernel_sharpen_2)
-output_3 = cv2.filter2D(img, -1, kernel_sharpen_3)
-cv2.imshow('Sharpening', output_1)
-cv2.imshow('Excessive Sharpening', output_2)
-cv2.imshow('Edge Enhancement', output_3)
-cv2.waitKey(0)
+import tkinter as tk
+from tkinter import filedialog
+
+selected_filter = None
+
+def apply_filter(filter_type):
+    global img, selected_filter
+    if img is not None:
+        if filter_type == "sharpen_1":
+            kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        elif filter_type == "sharpen_2":
+            kernel = np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1]])
+        elif filter_type == "sharpen_3":
+            kernel = np.array([[-1, -1, -1, -1, -1],
+                               [-1, 2, 2, 2, -1],
+                               [-1, 2, 8, 2, -1],
+                               [-1, 2, 2, 2, -1],
+                               [-1, -1, -1, -1, -1]]) / 8.0
+        else:
+            kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+
+        output = cv2.filter2D(img, -1, kernel)
+        cv2.imshow('Filtered Image', output)
+        selected_filter = filter_type
+
+def apply_effect(effect_type):
+    global img, selected_filter
+    if img is not None:
+        if effect_type == "grayscale":
+            output = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        elif effect_type == "invert_colors":
+            output = cv2.bitwise_not(img)
+        # Thêm hiệu ứng mới ở đây
+        cv2.imshow('Filtered Image', output)
+        selected_filter = effect_type
