a1ade18be76699ab1c8b12cff5647b6e01d499ae
NguyenVanTienManh <145360803+NguyenVanTienManh@users.noreply.github.com>
2023-10-26 20:51:05 +0700

    Update bt2.py

--- bt2.py
+++ bt2.py
@@ -35,3 +35,25 @@ def apply_effect(effect_type):
         # Thêm hiệu ứng mới ở đây
         cv2.imshow('Filtered Image', output)
         selected_filter = effect_type
+def save_image():
+    global img
+    if img is not None:
+        file_path = filedialog.asksaveasfilename(defaultextension=".jpg", filetypes=[("JPEG files", "*.jpg")])
+        if file_path:
+            cv2.imwrite(file_path, img)
+            print("Image saved successfully.")
+
+def open_image():
+    global img
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif *.tif")])
+    if file_path:
+        img = cv2.imread(file_path)
+        if img is not None:
+            cv2.imshow('Original', img)
+    root.after(1, update)
+
+def update():
+    key = cv2.waitKey(1)
+    if key == 27:
+        cv2.destroyAllWindows()
+    root.after(1, update)
