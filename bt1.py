60a96e3c98485b589e840bdf3c27d1d736b5759e
NguyenTu1703 <145697088+NguyenTu1703@users.noreply.github.com>
2023-10-30 23:40:32 +0700

    Update bt1.py
    them cua so giao dien

--- bt1.py
+++ bt1.py
@@ -4,14 +4,15 @@ import tkinter as tk
 from tkinter import filedialog
 
 selected_filter = None
+img = None  # Initialize img as None
 
 def apply_filter(filter_type):
-    global img, selected_filter
+    global selected_filter
     if img is not None:
         if filter_type == "sharpen_1":
-            kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+            kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1])
         elif filter_type == "sharpen_2":
-            kernel = np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1]])
+            kernel = np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1])
         elif filter_type == "sharpen_3":
             kernel = np.array([[-1, -1, -1, -1, -1],
                                [-1, 2, 2, 2, -1],
@@ -19,31 +20,46 @@ def apply_filter(filter_type):
                                [-1, 2, 2, 2, -1],
                                [-1, -1, -1, -1, -1]]) / 8.0
         else:
-            kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+            kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0])
 
         output = cv2.filter2D(img, -1, kernel)
         cv2.imshow('Filtered Image', output)
-        selected_filter = filter_type
+        selected_filter = filter_type  # Update the selected filter
+
 def apply_effect(effect_type):
-    global img, selected_filter
+    global selected_filter
     if img is not None:
         if effect_type == "grayscale":
             output = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         elif effect_type == "invert_colors":
             output = cv2.bitwise_not(img)
-            # Them hieu ung moi
         cv2.imshow('Filtered Image', output)
-        selected_filter = effect_type
+        selected_filter = effect_type  # Update the selected effect
 
-img = cv2.imread('anh1.JPG')
-rows, cols = img.shape[:2]
-kernel_identity = np.array([[0,0,0], [0,1,0], [0,0,0]])
-kernel_3x3 = np.ones((3,3), np.float32) / 9.0
-kernel_5x5 = np.ones((5,5), np.float32) / 25.0
-cv2.imshow('Original', img)
-output = cv2.filter2D(img, -1, kernel_identity)
-cv2.imshow('Identity filter', output)
-output = cv2.filter2D(img, -5, kernel_3x3)
-cv2.imshow('3x3 filter', output)
-output = cv2.filter2D(img, -1, kernel_5x5)
-cv2.imshow('5x5 filter', output)
+def open_file():
+    global img
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        img = cv2.imread(file_path)
+        cv2.imshow('Original', img)
+
+# Create a tkinter window
+root = tk.Tk()
+root.title("Image Filters")
+
+# Add buttons to apply filters
+sharpen_1_button = tk.Button(root, text="Sharpen 1", command=lambda: apply_filter("sharpen_1"))
+sharpen_2_button = tk.Button(root, text="Sharpen 2", command=lambda: apply_filter("sharpen_2"))
+sharpen_3_button = tk.Button(root, text="Sharpen 3", command=lambda: apply_filter("sharpen_3"))
+grayscale_button = tk.Button(root, text="Grayscale", command=lambda: apply_effect("grayscale"))
+invert_colors_button = tk.Button(root, text="Invert Colors", command=lambda: apply_effect("invert_colors"))
+open_file_button = tk.Button(root, text="Open Image", command=open_file)
+
+sharpen_1_button.pack()
+sharpen_2_button.pack()
+sharpen_3_button.pack()
+grayscale_button.pack()
+invert_colors_button.pack()
+open_file_button.pack()
+
+root.mainloop()  # Start the tkinter main loop
