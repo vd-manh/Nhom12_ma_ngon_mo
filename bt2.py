8bc96126645a8f5899d9b850849eae1271aa31c3
vd-manh <110422164+vd-manh@users.noreply.github.com>
2023-10-31 00:09:31 +0700

    bt2_done

--- bt2.py
+++ bt2.py
@@ -3,27 +3,16 @@ import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 
-selected_filter = None
+filters = {
+    "sharpen_1": np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]),
+    "sharpen_2": np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1]]),
+    "sharpen_3": np.array([[-1, -1, -1, -1, -1],
+                           [-1, 2, 2, 2, -1],
+                           [-1, 2, 8, 2, -1],
+                           [-1, 2, 2, 2, -1],
+                           [-1, -1, -1, -1, -1]]) / 8.0,
+}
 
-def apply_filter(filter_type):
-    global img, selected_filter
-    if img is not None:
-        if filter_type == "sharpen_1":
-            kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-        elif filter_type == "sharpen_2":
-            kernel = np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1]])
-        elif filter_type == "sharpen_3":
-            kernel = np.array([[-1, -1, -1, -1, -1],
-                               [-1, 2, 2, 2, -1],
-                               [-1, 2, 8, 2, -1],
-                               [-1, 2, 2, 2, -1],
-                               [-1, -1, -1, -1, -1]]) / 8.0
-        else:
-            kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
-
-        output = cv2.filter2D(img, -1, kernel)
-        cv2.imshow('Filtered Image', output)
-        selected_filter = filter_type
 selected_filters = []
 
 def apply_filters():
@@ -35,16 +24,7 @@ def apply_filters():
             if kernel is not None:
                 output = cv2.filter2D(output, -1, kernel)
         cv2.imshow('Filtered Image', output)
-def apply_effect(effect_type):
-    global img, selected_filter
-    if img is not None:
-        if effect_type == "grayscale":
-            output = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        elif effect_type == "invert_colors":
-            output = cv2.bitwise_not(img)
-        # Thêm hiệu ứng mới ở đây
-        cv2.imshow('Filtered Image', output)
-        selected_filter = effect_type
+
 def save_image():
     global img
     if img is not None:
@@ -73,11 +53,13 @@ def update():
     if key == 27:
         cv2.destroyAllWindows()
     root.after(1, update)
+
 root = tk.Tk()
 root.title("Image Filters")
 
 cv2.namedWindow('Original')
 cv2.namedWindow('Filtered Image')
+
 open_button = tk.Button(root, text="Open Image", command=open_image)
 open_button.pack()
 
@@ -93,5 +75,10 @@ sharpen_3_button.pack()
 apply_filters_button = tk.Button(root, text="Apply Filters", command=apply_filters)
 apply_filters_button.pack()
 
+save_button = tk.Button(root, text="Save Image", command=save_image)
+save_button.pack()
+
+img = None
+update()
 
 root.mainloop()
